871308122668b584bd4080ad95ff352eb9467346
Kajetan Terlecki <k.terlecki@wp.pl>
2020-01-09 10:58:40 +0100

    BUG with options titles into trip

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,11 +4,19 @@ import PropTypes from 'prop-types';
 
 import {Grid, Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = props => (
   console.log('orderform props', props),
   <Grid>
     <Row>
+      {pricing.map(option =>
+        <Col key={option.id} md={4}>
+          {console.log('option.id', option.id)}
+          <OrderOption {...option} />
+        </Col>
+      )}
       <Col xs={12}>
         <OrderSummary tripCost={props.tripCost} tripOptions={props.options}/>
       </Col>
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+
+
+const OrderOption = () => (
+  <div className={styles.component}>
+    <h3 className={styles.title}>{}</h3>
+  </div>
+);
+
+OrderOption.propTypes = {
+
+};
+
+export default OrderOption;
